101737410d00b4a83ed558d6d1c9ee3132297f80
liaolinan <943252723@qq.com>
2018-08-07 10:27:42 +0800

    合并代码

--- app/src/main/java/com/bclould/tea/utils/Constants.java
+++ app/src/main/java/com/bclould/tea/utils/Constants.java
@@ -12,7 +12,7 @@ import android.support.annotation.RequiresApi;
 public class Constants {
     public static final String ADMINISTRATOR_NAME = "TOPPERCHAT";
     public static final int BET_ARR_COUNT = 4;
-    private static final boolean isDebug = true;
+    private static final boolean isDebug = false;
     public static final String BASE_URL = isDebug ? "https://www.bclould.com:8112/" : "https://api.cnblocklink.com/";
     public static final String NEW_BASE_URL = isDebug ? "https://www.bclould.com:8112/" : "https://www.cnblocklink.com/";
     public static final String DOMAINNAME3 = isDebug ? "wss://socket.bclould.com:8179/toco_server" : "wss://socket.cnblocklink.com:2087/toco_server";
